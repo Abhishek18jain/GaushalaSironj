e795deecfecc91d85763aa7501e8c6786eb51676
jainshab1803 <abhijain1803g@gmail.com>
2025-06-22 14:53:12 +0530

    urlUpdate

--- app.js
+++ app.js
@@ -15,9 +15,7 @@ const Path = require("path");
 const engine = require('ejs-mate');
 const app = express();
 const port = 3000;
-const dbUrl = process.env.ATLASDB_URL;
- mongoose.connect(dbUrl, {
-}).then(() => {
+ mongoose.connect("mongodb://localhost:27017/").then(() => {
     console.log("Connected to MongoDB"
     );
 }).catch((err) => {
